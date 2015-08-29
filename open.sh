58f9eb67521f5131fec39e6e8d7127b87e9bfa0a
t-k- <clock126@126.com>
2015-08-28 22:39:39 -0400

    first commit

---
+++ open.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+[ ! $# -eq 1 ] && echo "usage: ${0} <feed-file-to-open>" && exit
+file="$1"
+[ ! -e ${file} ] && echo "bad file" && exit
+
+cat ${file} | grep -oP '[^ ]*/[^ ]*' > links
+cat links | grep -oP '(?<=/).*' > names
+cat names | sort | uniq -c | sort -r -k1 > cnt-uniq-names
+cat names | sort -u > only-uniq-names
+
+function open_and_delay {
+	google-chrome "https://github.com/${1}" &
+	sleep 1
+}
+
+i=0
+total=$(wc -l only-uniq-names)
+total=`echo $total | grep -o '^[0-9]*'`
+while read name 
+do
+	cnt_uniq_name=$(grep "\b${name}\b" cnt-uniq-names)
+	echo -n "${cnt_uniq_name} -> "
+	link=`grep -m1 ${name} links`
+	let 'i=i+1'
+	echo "${link}  #${i} / ${total}"
+	open_and_delay ${link}
+done << EOF
+$(cat only-uniq-names)
+EOF
+
+# rm -f links names only-uniq-names cnt-uniq-names
